e168d04ea3c090347befbcd8760ea5417b0c2a62
nogna <nogna@hotmail.se>
2017-07-15 20:50:05 +0200

    Second commit

---
+++ static/js/get_parameters.js
@@ -0,0 +1,15 @@
+/*
+This the first js
+*/
+
+function get_throughput(){
+    var throughput_val = document.getElementsByName('throughput');
+    console.log(throughput_val);
+    for (var i = 0, length = throughput_val.length; i < length; i++) {
+        if (throughput_val[i].checked) {
+            console.log(throughput_val[i]);
+
+        }
+    }
+
+}
