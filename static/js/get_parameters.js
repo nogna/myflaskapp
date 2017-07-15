6c24d979f81b259eaea6979bb23da401dc6231f1
nogna <nogna@hotmail.se>
2017-07-15 21:27:43 +0200

    Continued work

--- static/js/get_parameters.js
+++ static/js/get_parameters.js
@@ -4,10 +4,10 @@ This the first js
 
 function get_throughput(){
     var throughput_val = document.getElementsByName('throughput');
-    
+
     for (var i = 0, length = throughput_val.length; i < length; i++) {
         if (throughput_val[i].checked) {
-            console.log(throughput_val[i]);
+            console.log(throughput_val[i].value);
 
         }
     }
