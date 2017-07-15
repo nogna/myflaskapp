e19b222d68eb560eed0715fcad7077d56ec0ce10
nogna <nogna@hotmail.se>
2017-07-15 20:54:01 +0200

    Switched branch

--- static/js/get_parameters.js
+++ static/js/get_parameters.js
@@ -4,7 +4,7 @@ This the first js
 
 function get_throughput(){
     var throughput_val = document.getElementsByName('throughput');
-    console.log(throughput_val);
+    
     for (var i = 0, length = throughput_val.length; i < length; i++) {
         if (throughput_val[i].checked) {
             console.log(throughput_val[i]);
